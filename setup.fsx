4d2341d9fa85eaf3caaabe2b24006e1e0198f0b9
Kodai Matsumoto <nobita.0918@gmail.com>
2022-03-22 19:10:55 +0900

    :sparkles: カスタムフォーマッタを実装した

--- setup.fsx
+++ setup.fsx
@@ -1,11 +1,57 @@
 #r "nuget: Microsoft.Extensions.Logging"
 #r "nuget: Microsoft.Extensions.Logging.Console"
 
+open System
+open System.IO
 open System.Diagnostics
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Logging.Console
 
-let task name (logger: ILogger) filename args =
-    use _ = logger.BeginScope name
+type CustomConsoleFormatter() =
+    inherit ConsoleFormatter("envLogger")
+
+    override _.Write(logEntry, scopeProvider, textWriter) =
+        let message = logEntry.Formatter.Invoke(logEntry.State, logEntry.Exception)
+
+        if logEntry.Exception = null && message = null then
+            ()
+        else
+            let logLevel =
+                match logEntry.LogLevel with
+                | LogLevel.Trace -> "TRACE"
+                | LogLevel.Debug -> "DEBUG"
+                | LogLevel.Information -> "INFO"
+                | LogLevel.Warning -> "WARN"
+                | LogLevel.Error -> "ERROR"
+                | LogLevel.Critical -> "CRITICAL"
+                | _ -> failwith "Unexpected log level"
+
+            textWriter.Write('[')
+            textWriter.Write(DateTime.Now.ToString("yyyy-MM-ddTHH::mm:ssZ"))
+            textWriter.Write(' ')
+            textWriter.Write(logLevel)
+            textWriter.Write(' ')
+            textWriter.Write(logEntry.Category)
+
+            scopeProvider.ForEachScope(
+                (fun scope (state: TextWriter) ->
+                    state.Write("::")
+                    state.Write(scope)),
+                textWriter
+            )
+
+            textWriter.Write("] ")
+            textWriter.Write(message.Trim())
+            textWriter.Write(Environment.NewLine)
+
+type ILoggingBuilder with
+    member this.AddEnvLoggerConsole() =
+        this
+            .AddConsole(fun opts -> opts.FormatterName <- "envLogger")
+            .AddConsoleFormatter<CustomConsoleFormatter, ConsoleFormatterOptions>()
+
+let task (logger: ILogger) filename args =
+    use _ = logger.BeginScope $"{filename} {args}"
 
     let startInfo = ProcessStartInfo(filename, args)
     startInfo.RedirectStandardOutput <- true
@@ -35,18 +81,16 @@ let task name (logger: ILogger) filename args =
 
 let () =
     use loggerFactory =
-        LoggerFactory.Create (fun builder ->
-            builder.AddSimpleConsole(fun opts -> opts.IncludeScopes <- true)
-            |> ignore)
+        LoggerFactory.Create(fun builder -> builder.AddEnvLoggerConsole() |> ignore)
 
     let logger = loggerFactory.CreateLogger "setup"
 
     [ async {
           use _ = logger.BeginScope "apt"
-          do! task "update" logger "sudo" "apt-get update"
-          do! task "upgrade" logger "sudo" "apt-get upgrade"
-          do! task "autoclean" logger "sudo" "apt-get autoclean"
-          do! task "autoremove" logger "sudo" "apt-get autoremove"
+          do! task logger "sudo" "apt-get update"
+          do! task logger "sudo" "apt-get upgrade"
+          do! task logger "sudo" "apt-get autoclean"
+          do! task logger "sudo" "apt-get autoremove"
       }
       async { logger.LogInformation "Hello, world!" } ]
     |> Async.Parallel
